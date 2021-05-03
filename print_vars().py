592512550e64d0f956a8cdfc30d79f2da4bc1a22
Roman <37178784+r0mbeg@users.noreply.github.com>
2021-05-04 00:32:40 +0300

    Update print_vars().py

--- print_vars().py
+++ print_vars().py
@@ -1,13 +1,27 @@
+import sys
+
+class Vehicle():
+    def __init__(self):
+        pass
+
+def foo():
+    #tests:
+    a = 1
+    b = [1, 3, 2]
+    c = [[1, 3], 5]
+    d = "str"
+    v = Vehicle()
+    print_vars()
+
 def print_vars():
-    varlist = [None, bool, int, float, complex, list,
-               tuple, str, bytes, bytearray, memoryview,
-               set, frozenset, dict]
     dictionary = sys._getframe(1).f_locals
     for key, value in dictionary.items():
-        if (type(value) in varlist):
+        # In Python3 the module is called __builtins__
+        # In Python3.7 the module is called builtins
+        if (value.__class__.__module__ == 'builtins'):
             print(key, ": True")
-
         else:
             print(key, ": False")
 
+foo()
 
